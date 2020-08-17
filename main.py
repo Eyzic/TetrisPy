43bb5d625ced3cb4edad0add0a1c3bc953e2e840
Eyzic <einar@ingemarsson.nu>
2020-08-17 17:15:30 +0200

    Added levels and changing speed accordingly

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from ui import UI
 BLOCK_SIZE = 30
 PLAYAREA_BLOCK_WIDTH = 10
 PLAYAREA_BLOCK_HEIGHT = 20
-DOWN_SPEED = 30
 playarea_starting_point = (30,30)
 grid_thickness = 2
 move_speed = 1
@@ -15,7 +14,7 @@ clock = pygame.time.Clock()
 def setup_pygame():
     pygame.init()
     pygame.key.set_repeat(50)
-    return pygame.display.set_mode((440, 800))
+    return pygame.display.set_mode((500, 800))
 
 def setup_gameboard(screen):
     return GameBoard(screen, PLAYAREA_BLOCK_HEIGHT, PLAYAREA_BLOCK_WIDTH, playarea_starting_point, BLOCK_SIZE, grid_thickness)
@@ -28,6 +27,7 @@ def main():
     game_board = setup_gameboard(screen)
     ui = setup_UI(screen)
     game_loop = True
+    down_speed = 30
     fallspeed_counter = 0
     tetrimino_is_done = False
     score = 0
@@ -44,14 +44,16 @@ def main():
            if lines:
               game_board.color_lines(lines)
               score += level * BASE_POINT * len(lines)
-              game_board.remove_lines(lines)  
+              game_board.remove_lines(lines) 	  
               if score >= LEVEL_GAP:
                  level += 1
+                 if down_speed > 0:
+                    down_speed -= down_speed/2.0 
            tetrimino = Shape()   #Spawn a new shape at the top
            tetrimino.place((4,0), game_board)
            tetrimino_is_done = False
 
-        if fallspeed_counter == DOWN_SPEED and not tetrimino_is_done:
+        if fallspeed_counter == down_speed and not tetrimino_is_done:
             tetrimino_is_done = not tetrimino.move_down(game_board)
             fallspeed_counter = 0
 
@@ -72,6 +74,7 @@ def main():
                     tetrimino_is_done = not tetrimino.move_down(game_board)
         game_board.draw()
         ui.draw_score(score)
+        ui.draw_level(level)
         pygame.display.flip()
         clock.tick(60)
         fallspeed_counter += 1
--- ui.py
+++ ui.py
@@ -5,6 +5,12 @@ class UI:
       self.screen = screen
 
    def draw_score(self, score):
-      my_font = pygame.font.Font(None, 25)
-      surface = my_font.render("Score: " + str(score), 0,(255,255,255))
-      self.screen.blit(surface, (30,40)) 
+      self.draw("Score: " + str(score), 35, (255,255,255), (360,50))
+   def draw_level(self, level):
+      self.draw("Level: " + str(level), 35, (255,255,255), (360,20))
+
+   def draw(self, text, size, color, position):
+      self.screen.blit(pygame.Surface(position), position)
+      my_font = pygame.font.Font(None, size)
+      surface = my_font.render(str(text), 0, color)
+      self.screen.blit(surface, position) 
